2cd5a4f815f051a8f5d7a7c605ff8cd9a460471b
sudha-suryavanshi <sudha.suryavanshi@gmail.com>
2022-07-20 21:32:53 +0530

    First Commit

---
+++ src/main/java/com/telusko/secureapp/FirstProjectDemoApplication.java
@@ -0,0 +1,14 @@
+package com.telusko.secureapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstProjectDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FirstProjectDemoApplication.class, args);
+		System.out.println("Hello");
+	}
+
+}

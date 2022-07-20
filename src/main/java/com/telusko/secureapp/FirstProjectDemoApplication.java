bdaa95565fb13240f03b1b8e488edd0df220cd32
sudha-suryavanshi <sudha.suryavanshi@gmail.com>
2022-07-20 21:38:03 +0530

    Second commit

--- src/main/java/com/telusko/secureapp/FirstProjectDemoApplication.java
+++ src/main/java/com/telusko/secureapp/FirstProjectDemoApplication.java
@@ -9,6 +9,7 @@ public class FirstProjectDemoApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(FirstProjectDemoApplication.class, args);
 		System.out.println("Hello");
+		System.out.println("How are you");
 	}
 
 }
